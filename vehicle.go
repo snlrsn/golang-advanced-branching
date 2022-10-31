e3cb96a793c37723ecd942f9bf9d491609ea35f0
simon <93946211+snlrsn@users.noreply.github.com>
2022-10-31 15:23:52 +0100

    Completed module 3

--- vehicle.go
+++ vehicle.go
@@ -84,13 +84,6 @@ func init() {
 
 }
 
-func main() {
-
-	// Generate ratings for the different vehicles
-	generateRating()
-	// Print ratings for the different vehicles
-}
-
 func readJSONFile() Values {
 
 	jsonFile, err := os.Open("feedback.json")
@@ -109,7 +102,6 @@ func readJSONFile() Values {
 func generateRating() {
 
 	f := readJSONFile()
-	return f
 	for _, v := range f.Models {
 		var vehResult feedbackResult
 		var vehRating rating
@@ -143,9 +135,9 @@ func generateRating() {
 	}
 }
 
-func showRating(model) string {
+func showRating(model string) {
 
-	var ratingFound = false
+	ratingFound := false
 	for m, r := range vehicleResult {
 		if m == model {
 			fmt.Printf("Total Ratings:%v\tPositive:%v\tNegative:%v\tNeutral:%v", r.feedbackTotal, r.feedbackPositive, r.feedbackNegative, r.feedbackNeutral)
@@ -153,7 +145,42 @@ func showRating(model) string {
 		}
 	}
 	if !ratingFound {
-		fmt.Printf("No rating for this vehicle")
+		fmt.Printf("No rating found for this vehicle")
 	}
-	return model
+}
+
+func (c *car) carDetails() {
+	fmt.Printf("\n%-5v: %-8v: %-12v ", "Car", c.make, c.model)
+	showRating(c.model)
+}
+
+func (b *bike) bikeDetails() {
+	fmt.Printf("\n%-5v: %-8v: %-12v ", "Bike", b.make, b.model)
+	showRating(b.model)
+}
+
+func (t *truck) truckDetails() {
+	fmt.Printf("\n%-5v: %-8v: %-12v ", "Truck", t.make, t.model)
+	showRating(t.model)
+}
+
+func main() {
+
+	// Generate ratings for the different vehicles
+	generateRating()
+	// Print ratings for the different vehicles
+	for _, veh := range inventory {
+
+		switch v := veh.(type) {
+		case car:
+			v.carDetails()
+		case bike:
+			v.bikeDetails()
+		case truck:
+			v.truckDetails()
+		default:
+			fmt.Printf("Are you sure this Vehicle Type exists")
+		}
+
+	}
 }
